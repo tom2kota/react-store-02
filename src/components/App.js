98ffcf88b34d5301dc7da649198bc0aa70616ab9
Web Dev <tom2kota@gmail.com>
2020-08-30 12:33:16 +0300

    Fix test

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import {HomePage} from "./pages/homepage/homePage";
-import {CategoryOne} from "./components/categories/categoryOne";
-import {CategoryTwo} from "./components/categories/categoryTwo";
-import {CategoryThree} from "./components/categories/categoryThree";
-import {CategoryFour} from "./components/categories/categoryFour";
-import {CategoryFive} from "./components/categories/categoryFive";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {HomePage} from "../pages/homepage/homePage";
+import {CategoryOne} from "./categories/categoryOne";
+import {CategoryTwo} from "./categories/categoryTwo";
+import {CategoryThree} from "./categories/categoryThree";
+import {CategoryFour} from "./categories/categoryFour";
+import {CategoryFive} from "./categories/categoryFive";
 
 export default function App() {
     return (
         <div>
+            <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={HomePage}/>
                 <Route path="/category_1" component={CategoryOne}/>
@@ -18,6 +19,7 @@ export default function App() {
                 <Route path="/category_4" component={CategoryFour}/>
                 <Route path="/category_5" component={CategoryFive}/>
             </Switch>
+            </BrowserRouter>
         </div>
     );
 }
