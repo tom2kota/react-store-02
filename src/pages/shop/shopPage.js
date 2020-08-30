b79a87ba3bf742114db0420f30a719b5a2f1b23a
Web Dev <tom2kota@gmail.com>
2020-08-30 13:33:09 +0300

    Render shop data

--- src/pages/shop/shopPage.js
+++ src/pages/shop/shopPage.js
@@ -1,12 +1,24 @@
 import React, {Component} from "react";
 import './shopPage.scss';
+import {SHOP_DATA} from './shopData.js'
+import {CollectionPreview} from "../../components/collection-preview/CollectionPreview";
 
 export class ShopPage extends Component {
 
+    state = {
+        collections: SHOP_DATA
+    }
+
     render() {
+        const {collections} = this.state;
+
         return (
-            <div>
-                <h1>Shop Page</h1>
+            <div className='shop-page'>
+                {
+                    collections.map(
+                        ({id, ...otherCollectionProps}) => <CollectionPreview key={id} {...otherCollectionProps}  />
+                    )
+                }
             </div>
         )
     }
